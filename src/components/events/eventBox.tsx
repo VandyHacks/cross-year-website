8fad25316c0c9146c9dfa6794b025703e1208681
ashwinkumargb <ashwinkumargb@gmail.com>
2020-07-02 15:57:52 -0500

    Timeline that doesn't work

--- src/components/events/eventBox.tsx
+++ src/components/events/eventBox.tsx
@@ -1,27 +1,123 @@
 import React from "react"
+import styled from "astroturf";
+
 import events from "./events.json"
 
-interface EventCheck {
-    Title: string;
-    Date: string;
-    Description: string;
-}
+const T1BG = styled("div")`
+  transform: translate3d(0, 0, 0);
+  position: absolute;
+  width: 100%; height: 100%;
+  top: 0; left: 0;
+  background-size: cover;
+  background-position: center center;
+  transition: filter 0.5s ease;
+  filter: grayscale(100%);
+`;
 
-let eventArray :EventCheck[] = events;
+const T1Year = styled("div")`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  z-index: 1;
+  border-top: 1px solid #fff;
+  border-bottom: 1px solid #fff;
+  
+  p {
+    font-family: 'Pathway Gothic One',Helvetica Neue,Helvetica,Arial,sans-serif;
+    font-size: 1.728rem;
+    line-height: 0;
+  }
+`;
+
+const T1Content = styled("div")`
+  transform: translate3d(0, 0, 0) translateY(25px);
+  position: relative;
+  z-index: 1;
+  text-align: center;
+  margin: 0 1.618em;
+  top: 55%;
+  opacity: 0;
+  
+  h1 {
+    font-family: 'Pathway Gothic One',Helvetica Neue,Helvetica,Arial,sans-serif;
+    text-transform: uppercase;
+    color: #1779cf;
+    font-size: 1.44rem;
+    font-weight: normal;
+  }
+`;
+
+const T1Item = styled("div")`
+  transform: translate3d(0, 0, 0);
+  position: relative;
+  width: 25%;
+  height: 100vh;
+  min-height: 600px;
+  color: #fff;
+  overflow: hidden;
+  transition: width 0.5s ease;
+  
+  &:before, &:after {
+    transform: translate3d(0, 0, 0);
+    content: '';
+    position: absolute;
+    left: 0; top: 0;
+    width: 100%; height: 100%;
+  }
+
+  &:after {
+    background: transparentize(#031625, 0.15);
+    opacity: 1;
+    transition: opacity 0.5s ease;
+  }
+
+  &:before {
+    background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,1) 75%);
+    z-index: 1;
+    opacity: 0;
+    transform: translate3d(0, 0, 0) translateY(50%);
+    transition: opacity 0.5s ease, transform 0.5s ease;
+  }
+
+  &:hover {
+    width: 30% !important;
+    
+    &:after {
+      opacity: 0;
+    }
+
+    &:before {
+      opacity: 1;
+      transform: translate3d(0, 0, 0) translateY(0);
+      transition: opacity 1s ease, transform 1s ease 0.25s;
+    }
+    
+    ${T1Content} {
+      opacity: 1;
+      transform: translateY(0);
+      transition: all 0.75s ease 0.5s;
+    }
+
+    ${T1BG} {
+      filter: grayscale(0);
+    }
+  }
+`;
+
+const Timeline = styled("section")`
+    display: flex;
+    background-color: #031625;
+
+    ${T1Item}:hover &{
+        width: 23.3333%;
+    }
+`;
 
 
-const EventsBox: React.FC<{}> = () => (
-    <div>
-        {eventArray.map(value => {
-            return (
-            <div>
-                <h4>{value.Title}</h4>
-                <li>Date: {value.Date}</li>
-                <li>Description: {value.Description}</li>
-            </div>
-            )
-        })}
-    </div>  
-)
 
-export default EventsBox;
+export const EventsBox: React.FC<{}> = () => {
+    return (
+        <Timeline></Timeline>
+    );  
+};
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,14 +1,29 @@
 import React from "react"
+import styled from "astroturf";
 
-import EventsBox from "../components/events/eventBox"
+import {EventsBox} from "../components/events/eventBox"
 import {PageLinks} from "../components/PageLinks"
+import {Footer} from "../components/Footer"
+
+const Main = styled("body")`
+  width: 100vw;
+  height: 100vh;
+  background-color: #0000DE;
+  display:grid;
+  grid-template-columns: 100vw;
+  grid-template-rows: 8vh 75vh 13vh;
+  background-image: url("../images/Big_VH_Logo.png");
+  background-repeat: no-repeat;
+  background-size: 40%; 
+`;
+
 
 const Events: React.FC<{}> = () => (
-    <div>
-        <h1>Events Page</h1>
-        <EventsBox />
+    <Main>
         <PageLinks />
-    </div>
+        <EventsBox />
+        <Footer />
+    </Main>
 )
 
 export default Events;
