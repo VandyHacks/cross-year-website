b766f628593f26084e071156e278870b227c1bce
ashwinkumargb <ashwinkumargb@gmail.com>
2020-08-20 21:27:40 -0500

    timeline with solid color

--- src/pages/2019_Events/Events2019.tsx
+++ src/pages/2019_Events/Events2019.tsx
@@ -1,27 +1,20 @@
 import React from "react"
 import styled from "astroturf";
-import {HelloWorld} from "./HelloWorld"
-import {VandyHacksVI} from "./VandyHacksVI"
+import {HelloWorld} from "./HelloWorld-Copy"
+import {VandyHacksVI} from "./VandyHacksVI-Copy"
 import {PageLinks} from "../../components/PageLinks";
 import {Footer} from "../../components/Footer";
 import "./2019Events.scss"
 
-
 export const Events2019: React.FC<{}> = () => (
-    <div className="timeline-container" id="timeline-1">
+    <div className="timeline-header">
     <PageLinks />
-    <div className="timeline">
-        <div className="timeline-item" data-text="FATHER OF THE TURKS">
-        <div className="timeline__content"><img className="timeline__img" src="http://i.cdn.ensonhaber.com/resimler/diger/ataturk_3473.jpg"/>
-            <h2 className="timeline__content-title">1881</h2>
-            <p className="timeline__content-desc">He was born in 1881 (probably in the spring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and strong-willed woman, raised him and his sister.</p>
-        </div>
-        </div>
-        <div className="timeline-item" data-text="FATHER OF THE TURKS">
-        <div className="timeline__content"><img className="timeline__img" src="http://gazetemanifesto.com/wp-content/uploads/2015/11/mustafa-kemal.jpg"/>
-            <h2 className="timeline__content-title">1893</h2>
-            <p className="timeline__content-desc">First enrolled in a traditionalreligious school, he soon switched to a modern school. In 1893, he entered a military highschool where his mathematics teacher gave him the second name Kemal (meaning perfection)in recognition of young Mustafa's superior achievement.</p>
-        </div>
+    <div className="timeline-container" id="timeline-1">
+        <div className="timeline">
+            <HelloWorld />
+            <div id="HelloWorld" >
+            <VandyHacksVI />
+            </ div>
         </div>
     </div>
     <Footer />
---
+++ src/pages/2019_Events/HelloWorld-Copy.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import styled from "astroturf";
+
+
+import events from "../../components/events/events.json"
+import "./2019Events.scss"
+
+var name:string = "HelloWorld";
+var title:string =  events[name].Title;
+var event1Description:string = events[name].Event1;
+var event2Description:string = events[name].Event2;
+var event1title:string = events[name].Event1Title;
+var event2title:string = events[name].Event2Title;
+var bgcolor:string = events[name].bgcolor;
+var image1:string = events[name].Image1;
+var image2:string = events[name].Image2;
+var BackgroundImage:string = events[name].BackgroundImage;
+var scroll : string = events[name].scroll;
+
+export const HelloWorld: React.FC<{}> = () => (
+  <div className="timeline">
+    <div className="timeline-item" data-text={title}>
+    <div className="timeline__content"><img className="timeline__img" src={image1}/>
+        <h2 className="timeline__content-title">{event1title}</h2>
+        <p className="timeline__content-desc">{event1Description}</p>
+    </div>
+    </div>
+    <div className="timeline-item" data-text={title}>
+      <div className="timeline__content"><img className="timeline__img" src={image2}/>
+          <h2 className="timeline__content-title">{event2title}</h2>
+          <p className="timeline__content-desc">{event2Description}</p>
+      </div>
+    </div>
+  </div>
+);
+
