9c725734202b1dad36283e3db779a3bd64db9253
xuezzou <ilfam1999@gmail.com>
2020-11-24 17:19:55 -0500

    add uncommited changes first to have a cleaner comparison later

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('D:/Captain/Documents/Spring 2020/VandyHacks/cross-year-website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/zouxue/Desktop/internship3.0/VandyHacksDev2.0/cross-year-website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('D:/Captain/Documents/Spring 2020/VandyHacks/cross-year-website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/zouxue/Desktop/internship3.0/VandyHacksDev2.0/cross-year-website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
