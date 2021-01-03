ce2934e37eca853b9977d03155d44a1daf3958e3
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-03 15:56:28 -0500

    Start eventTemplate usage. Will put into a TODO for later

--- src/components/events/eventTemplate.tsx
+++ src/components/events/eventTemplate.tsx
@@ -1,34 +1,34 @@
 import React from "react"
-import styled from "astroturf";
+import {PageLinks} from "../../components/PageLinks";
+import {Footer} from "../../components/Footer";
+import "../../components/events/events.scss"
+import totalevents from '../../components/events/events.json';
 
-import events from "./events.json"
-import "./events.scss"
 
-export const HelloWorld: React.FC<{ title : string, image: string, desc : string }> = 
-  ({ title, image, desc }) => {
+// TODO: This is the template that should be used to generate the different event pages. Right now the pages are all basically 1 character apart and hardcoded.
+const eventTemplate: React.FC<{}> = () => {
+    const timelineItems = totalevents[0].Information.map((item) => (
+        <div className="timeline-item" data-text={item.Title}>
+            {/* TODO: Kinda dumb but you need to have a bit of the string static or else Webkit starts to complain */}
+            <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${item.Image}`)}/>
+                <p className="timeline__content-desc">{item.Description}</p>
+            </div>
+        </div>
+    ));
     return(
-  <div className="timeline">
-    <div className="timeline-item" data-text={title}>
-      {/* TODO: Kinda dumb but you need to have a bit of the string static or else Webkit starts to complain */}
-      <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${image}`)}/>
-          <p className="timeline__content-desc">{desc}</p>
-      </div>
-    </div>
-    <div className="timeline-item" data-text={title}>
-      {/* TODO: Kinda dumb but you need to have a bit of the string static or else Webkit starts to complain */}
-      <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${image}`)}/>
-          <h2 className="timeline__content-title">{title}</h2>
-          <p className="timeline__content-desc">{desc}</p>
-      </div>
-    </div>
-    <div className="timeline-item" data-text={title}>
-      {/* TODO: Kinda dumb but you need to have a bit of the string static or else Webkit starts to complain */}
-      <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${image}`)}/>
-          <h2 className="timeline__content-title">{title}</h2>
-          <p className="timeline__content-desc">{desc}</p>
-      </div>
-    </div>
-  </div>
+        <body style={{backgroundImage: "None", backgroundColor: totalevents[0].BackgroundColor}}>
+            <div className="timeline-header">
+                <PageLinks />
+                <div className="timeline-container" style={{backgroundColor: `${totalevents[0].BackgroundColor}`}}>
+                    {/* TODO: This should REALLY be in a map function but I couldn't get it working so here we are with terrible code */}
+                    <div className="timeline">
+                        {timelineItems}
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        </body>
     );
-    };
+};
 
+export default eventTemplate; 
