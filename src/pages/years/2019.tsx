093635513cc9ae733d1e9600c9ef4da8e0c3bb92
SamLee514 <samuel.r.lee@vanderbilt.edu>
2021-01-03 15:49:42 -0500

    make all event pages programmatic

--- src/pages/years/2019.tsx
+++ src/pages/years/2019.tsx
@@ -5,6 +5,14 @@ import "../../components/events/events.scss"
 import totalevents from '../../components/events/events.json';
 
 const events2019: React.FC<{}> = () => {
+    const timelineItems = totalevents[1].Information.map((item) => (
+        <div className="timeline-item" data-text={item.Title}>
+            {/* TODO: Kinda dumb but you need to have a bit of the string static or else Webkit starts to complain */}
+            <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${item.Image}`)}/>
+                <p className="timeline__content-desc">{item.Description}</p>
+            </div>
+        </div>
+    ));
     return(
         <body style={{backgroundImage: "None", backgroundColor: totalevents[1].BackgroundColor}}>
             <div className="timeline-header">
@@ -12,22 +20,7 @@ const events2019: React.FC<{}> = () => {
                 <div className="timeline-container" style={{backgroundColor: `${totalevents[1].BackgroundColor}`}}>
                     {/* TODO: This should REALLY be in a map function but I couldn't get it working so here we are with terrible code */}
                     <div className="timeline">
-                        <section className="timeline-item" data-text={totalevents[1].Information[0].Title} id={totalevents[1].Information[0].Title}>
-                            {/* TODO: Kinda dumb but you need to have a bit of the string static or else Webkit starts to complain */}
-                            <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${totalevents[1].Information[0].Image}`)}/>
-                                <p className="timeline__content-desc">{totalevents[1].Information[0].Description}</p>
-                            </div>
-                        </section>
-                        <section className="timeline-item" data-text={totalevents[1].Information[1].Title } id={totalevents[1].Information[1].Title}>
-                            <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${totalevents[1].Information[1].Image}`)}/>
-                                <p className="timeline__content-desc">{totalevents[1].Information[1].Description}</p>
-                            </div>
-                        </section>
-                        <section className="timeline-item" data-text={totalevents[1].Information[2].Title} id={totalevents[1].Information[2].Title}>
-                            <div className="timeline__content"><img className="timeline__img" src={require(`../../images/${totalevents[1].Information[2].Image}`)}/>
-                                <p className="timeline__content-desc">{totalevents[1].Information[2].Description}</p>
-                            </div>
-                        </section>
+                        {timelineItems}
                     </div>
                 </div>
                 <Footer />
